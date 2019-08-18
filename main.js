56eb11464419625f74897c7cbab61e48b3d5dfc8
Thomas Doyle <thomasdoyle@MacBook-Pro.lan>
2019-08-18 13:00:56 +1000

    First commit

--- main.js
+++ main.js
@@ -1,26 +1,35 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
-const path = require('path')
+const {app, BrowserWindow} = require('electron');
+const path = require('path');
+const url = require('url');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
-function createWindow () {
+function createWindow() {
   // Create the browser window.
+
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
+    // webPreferences: {
+    //   preload: path.join(__dirname, 'preload.js')
+    // }
   })
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  // mainWindow.loadFile('index.html')
+
+  // and load the index.html of the app.
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
 
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
--- renderer.js
+++ renderer.js
@@ -1,3 +1,40 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+const fs = require('fs')
+const marked = require('marked')
+const hljs = require('highlight.js')
+const remote = require('electron').remote
+
+const { dialog } = require('electron').remote
+
+const readFile = (file) => {
+  fs.readFile(file, (err, data) => {
+    if (err) {
+      console.log(err.message);
+    } else {
+      document.querySelector('.md').innerHTML = marked(data.toString())
+      Array.from(document.querySelectorAll('pre code')).forEach(
+        block => hljs.highlightBlock(block)
+      )
+      }
+  })
+}
+
+const filters = { filters: [{ name: 'Markdown', extensions: ['md', 'markdown'] }] }
+
+const openFilePicker = () => {
+  dialog.showOpenDialog(filters, fileNames => {
+    if (fileNames) {
+      readFile(fileNames[0])
+    }
+  })
+}
+
+const close = e => {
+  const window = remote.getCurrentWindow()
+  window.close()
+}
+
+document.querySelector('.close').addEventListener('click', close)
+document.querySelector('.select-file').addEventListener('click', openFilePicker)
